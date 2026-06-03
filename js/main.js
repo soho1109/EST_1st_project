b9a81dd6b1a419c32e7c8b8673b66c3f42987b34
soho1109 <park1109pp@naver.com>
2026-06-03 19:29:37 +0900

    feat: 슬라이드 추가 html/css수정

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// popup
+const popup = document.querySelector("#popup");
+      const popupCloseBtn = popup.querySelector("#popup-Btn");
+      const agree = document.querySelector("#agree");
+
+      popup.style.display = document.cookie.includes("popup = 안볼래")
+        ? "none"
+        : "block";
+
+      popupCloseBtn.addEventListener("click", () => {
+        popup.style.display = "none";
+        if (agree.checked) {
+          createCookie("popup", "안볼래", 1);
+        } else {
+          createCookie("popup", "안볼래", -1);
+        }
+      });
+
+      function createCookie(name, value, days) {
+        let today = new Date();
+
+        today.setDate(today.getDate() + days);
+
+        document.cookie = `${name}=${value}; Expires=${today.toUTCString()}`;
+      }
+// Slide     
+const controlSlide = new Swiper(".online-content", {
+  loop: true,
+
+  navigation: {
+    nextEl: ".slides_nav .next",
+    prevEl: ".slides_nav .prev",
+  },
+
+   pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 20,
+});
